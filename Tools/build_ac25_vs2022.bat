322790eb6bd3f5fa83c6889b8940afe98dc01ed6
kovacsv <viktorkovacs@gmail.com>
2023-09-30 18:11:20 +0200

    Update source structure.

--- Tools/build_ac25_vs2022.bat
+++ Tools/build_ac25_vs2022.bat
@@ -1,5 +1,5 @@
 pushd %~dp0
-cmake -B ../Build/AC25 -G "Visual Studio 17 2022" -A "x64" -T "v142" -DAC_API_DEVKIT_DIR="C:\Program Files\GRAPHISOFT\API Development Kit 25.3002" .. || goto :error
+cmake -B ../Build/AC25 -G "Visual Studio 17 2022" -A "x64" -T "v142" -DAC_API_DEVKIT_DIR="C:\Program Files\GRAPHISOFT\API Development Kit 25.3002\Support" .. || goto :error
 cmake --build ../Build/AC25 --config Debug || goto :error
 cmake --build ../Build/AC25 --config RelWithDebInfo || goto :error
 popd
