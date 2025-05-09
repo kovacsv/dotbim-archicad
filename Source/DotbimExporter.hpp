5a6f4274500c3b0f63316e5291f65d7ac457138a
kovacsv <viktorkovacs@gmail.com>
2025-05-09 20:48:09 +0200

    Remove source code.

--- Source/DotbimExporter.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <Model.hpp>
-
-std::string ExportDotbim (const ModelerAPI::Model& model);
-GSErrCode ExportDotbimFile (const ModelerAPI::Model& model, const IO::Location& location);
-GSErrCode ExportDotbimFileFrom3DWindow (const IO::Location& location);
--- Source/DotbimImporter.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <Location.hpp>
-
-GSErrCode ImportDotbim (const IO::Location& fileLocation, const GS::UniString& materialNameTemplate);
